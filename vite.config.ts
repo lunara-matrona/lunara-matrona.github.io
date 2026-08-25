c00aa9ab1d147462c4b1ed395918e88d87e8cb0d
Bruno Toro <bruno.visualmaster@gmail.com>
2026-08-25 15:52:55 -0400

    modificaciones para el repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-
-  // En localhost usamos la raíz.
-  // En producción usamos la ruta del repositorio de GitHub Pages.
-  base: mode === "production" ? "/portfolio_sami/" : "/",
-}));
+  base: "./",
+});
